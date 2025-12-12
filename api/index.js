d63356c7ce10879936a1a1e166d9822cd5fdbd28
lomanto215 <triasajilomanto215@gmail.com>
2025-12-12 20:57:19 +0700

    Configure CORS with specific options

--- api/index.js
+++ api/index.js
@@ -20,7 +20,11 @@ const app = express()
 const port = process.env.PORT || 3000
 
 app.use(helmet())
-app.use(cors())
+app.use(cors({
+  origin: "*",
+  methods: "GET,POST,PUT,DELETE",
+  allowedHeaders: "Content-Type, Authorization"
+}));
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(morgan(process.env.NODE_ENV === 'production' ? 'combined' : 'dev'))
