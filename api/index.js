887e6b2cc320692ca6cc4bb5d39184003e006fea
lomanto215 <triasajilomanto215@gmail.com>
2025-12-13 00:57:31 +0700

    Disable helmet and CORS middleware
    Comment out helmet and CORS middleware for now

--- api/index.js
+++ api/index.js
@@ -19,12 +19,12 @@ const pool = require('./config/db');
 const app = express()
 const port = process.env.PORT || 3000
 
-app.use(helmet())
-app.use(cors({
-  origin: "*",
-  methods: "GET,POST,PUT,DELETE,OPTIONS",
-  allowedHeaders: "Content-Type, Authorization"
-}));
+// app.use(helmet())
+// app.use(cors({
+//   origin: "*",
+//   methods: "GET,POST,PUT,DELETE,OPTIONS",
+//   allowedHeaders: "Content-Type, Authorization"
+// }));
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(morgan(process.env.NODE_ENV === 'production' ? 'combined' : 'dev'))
