7422deca10e4c4083104a0658bbfd09fc6ef3ac7
lomanto215 <triasajilomanto215@gmail.com>
2025-12-12 21:04:59 +0700

    Simplify CORS configuration in index.js

--- api/index.js
+++ api/index.js
@@ -20,11 +20,7 @@ const app = express()
 const port = process.env.PORT || 3000
 
 app.use(helmet())
-app.use(cors({
-  origin: "*",
-  methods: "GET,POST,PUT,DELETE",
-  allowedHeaders: "Content-Type, Authorization"
-}));
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(morgan(process.env.NODE_ENV === 'production' ? 'combined' : 'dev'))
