2127d3758bdddf8ead84e910cf5607d0da066a1a
lomanto215 <triasajilomanto215@gmail.com>
2025-12-13 12:26:25 +0700

    fix: cors

--- api/index.js
+++ api/index.js
@@ -26,15 +26,14 @@ app.use(
     origin: "*",
   })
 );
-app.options("*", cors());
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE,OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type,Authorization");
+  if (req.method === "OPTIONS") return res.sendStatus(204);
   next();
 });
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan(process.env.NODE_ENV === "production" ? "combined" : "dev"));
