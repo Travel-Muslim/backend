288eece1a469b669b6f6f787d86be57bbcf743fb
lomanto215 <triasajilomanto215@gmail.com>
2025-12-13 12:24:10 +0700

    fix: options cors and header

--- api/index.js
+++ api/index.js
@@ -26,6 +26,15 @@ app.use(
     origin: "*",
   })
 );
+app.options("*", cors());
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE,OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type,Authorization");
+  next();
+});
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan(process.env.NODE_ENV === "production" ? "combined" : "dev"));
